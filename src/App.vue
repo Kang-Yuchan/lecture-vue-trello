225a9395bfebaa107c83f7ef8da6e7b0a2d3e8f7
Kang.Yuchan <ikimaku0517@gmail.com>
2019-02-20 16:06:16 +0900

    start trello project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    ここからコードを書きます！
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const Login = {template: '<div>ログインページ</div>'}
+const NotFound = {template: "<div>ページが見つかりませんでした。"}
+
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: App},
+    { path: '/login', component: Login},
+    { path: '*', component: NotFound }
+  ]
+  
+})
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h({template: '<router-view></router-view>'})
+})
