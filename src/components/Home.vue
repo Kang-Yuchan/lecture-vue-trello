8d8aebf93f323c9a3bb02ae3119932924a10365d
Kang.Yuchan <ikimaku0517@gmail.com>
2019-02-20 19:12:31 +0900

    board api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,59 +1,49 @@
 <template>
+  <div>
+    Home
     <div>
-        Home
-        <div>
-            Board List:
-            <div v-if="loading">loading...</div>
-            <div v-else>
-                API result: <pre>{{apiRes}}</pre>
-            </div>
-            <div v-if="error"><pre>{{error}}</pre></div>
-            <ul>
-                <li>
-                    <router-link to="/b/1">Board 1</router-link>
-                </li>
-                 <li>
-                    <router-link to="/b/2">Board 2</router-link>
-                </li>
-            </ul>
+      Board List: 
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        <div v-for="b in boards" :key=b.id>
+          <pre>{{b}}</pre>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 
 export default {
-    data() {
-        return {
-            loading: false,
-            apiRes: '',
-            error: ''
-        }
-    },
-    created() {
-        this.fetchData()
-    },
-    methods: {
-        fetchData() {
-            this.loading = true
-
-            axios.get('http://localhost:3000/health')
-                .then(res => {
-                    this.apiRes = res.data
-                })
-                .catch(res => {
-                    this.error = res.response.data
-                })
-                .finally(() => {
-                    this.loading = false
-                })
-
-        }
+  data() {
+    return {
+      loading: false,
+      boards: '',
+      error: ''
     }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+      axios.get('http://localhost:3000/boards')
+        .then(res => {
+          this.boards = res.data
+        })
+        .catch(res => {
+          this.$router.replace('/login')
+        })
+        .finally(_=> {
+          this.loading = false
+        })
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
