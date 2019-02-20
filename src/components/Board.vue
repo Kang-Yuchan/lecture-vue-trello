01e28e538938adc98798f508033009f36e8be43d
Kang.Yuchan <ikimaku0517@gmail.com>
2019-02-20 17:10:06 +0900

    dynamic routing

---
+++ src/components/Board.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        Board
+        <div>bid: {{bid}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        this.bid=this.$route.params.bid
+    }
+}
+</script>
+
+<style>
+
+</style>
